61eccaec2ef79c1ef392cf1c12dab9b33e769ebe
Zerapium <mayurhiwale2451982@gmail.com>
2024-01-31 22:46:08 +0530

    Add context for 'differentiate between' type questions

--- ai.mjs
+++ ai.mjs
@@ -10,7 +10,7 @@ const genAI = new GoogleGenerativeAI(process.env.AI_KEY);
 async function run(query, context) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const prompt = context ? query + " [Context = " + "computer science" + "]" : query + " [Context = " + "computer science" + "]";
+  if(query.toLowerCase().includes("differentiate") || query.toLowerCase().includes("differentiate")) query +="(10 points table)"
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
